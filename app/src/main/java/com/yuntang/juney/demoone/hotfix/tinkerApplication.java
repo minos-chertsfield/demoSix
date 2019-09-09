192a8e3978f5c9afc9c73d6ded6264f3012cb9b4
minos-chertsfield <1291197472@qq.com>
2019-09-09 11:33:13 +0800

    "加入热修复依赖，随后加入该功能"

---
+++ app/src/main/java/com/yuntang/juney/demoone/hotfix/tinkerApplication.java
@@ -0,0 +1,20 @@
+package com.yuntang.juney.demoone.hotfix;
+
+import android.app.Application;
+
+public class tinkerApplication extends Application {
+
+
+//    private ApplicationLike applicationLike;
+//
+//    @Override
+//    public void onCreate() {
+//        super.onCreate();
+//
+//
+//        if (BuildConfig.DEBUG) {
+//
+//
+//        }
+//    }
+}
